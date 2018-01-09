83f07b189c43974d70010fc0c2b714dfe5bc2347
karthik chinni <kaarthik.chinni@gmail.com>
2018-01-10 01:58:29 +0530

    Sorted DLL Null Pointer exception

---
+++ src/com/ck/linkedlist/SortedDLLInsert.java
@@ -0,0 +1,40 @@
+package com.ck.linkedlist;
+
+class Node {
+	int data;
+	Node next;
+	Node prev;
+}
+
+public class SortedDLLInsert {
+	Node SortedInsert(Node head, int data) {
+		Node p = new Node();
+		p.data = data;
+
+		Node q = head;
+		Node previousNode = null;
+
+		if (head == null) {// null list
+			return p;
+		}
+		while (q != null && q.data < data) {
+			q = q.next;
+			previousNode = q.prev;
+		}
+
+		if (previousNode == null) {// First element
+			q.next = p;
+			p.prev = q;
+			return q;
+		} else if (q == null) {// Last element
+			previousNode.next = p;
+			p.prev = previousNode;
+		} else {
+			previousNode.next = p;
+			p.prev = previousNode;
+			p.next = q;
+			q.prev = p;
+		}
+		return head;
+	}
+}
