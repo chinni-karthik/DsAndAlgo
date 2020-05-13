f4d151c527c984f3490885ae02a7b8c41e5a687b
chinni-karthik <chinni.kaarthik@gmail.com>
2020-05-13 05:56:59 +0530

    Update Employee.java

--- src/com/ck/pre/ds/Employee.java
+++ src/com/ck/pre/ds/Employee.java
@@ -1,9 +1,11 @@
 package com.ck.pre.ds;
 
 public class Employee {
+	int a;
+	static int b;
+
 	public static void main(String[] args) {
-		Employee e1 = new Employee();
-		Employee e2 = new Employee();
-		e1 = e2;
+		Employee e = new Employee();
+		e.a = 10;
 	}
 }
