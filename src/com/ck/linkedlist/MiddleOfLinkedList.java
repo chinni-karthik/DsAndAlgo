a8237133933c8fa84955d2556506fb2ae42d6a18
chinni-karthik <chinni.kaarthik@gmail.com>
2020-05-18 10:55:22 +0530

    making corrections

--- src/com/ck/linkedlist/MiddleOfLinkedList.java
+++ src/com/ck/linkedlist/MiddleOfLinkedList.java
@@ -2,11 +2,11 @@ package com.ck.linkedlist;
 
 public class MiddleOfLinkedList {
 	private int getMiddleElementOfLinkedList(Node head) {
-		if (head != null)
+		if (head == null)//Base condition
 			return 0;
 		Node p = head;// slow pointer
 		Node q = head;// fast pointer
-		while (q != null) {
+		while (q.next.next != null) {
 			p = p.next;
 			q = q.next.next;
 		}
--- src/com/ck/pre/ds/Employee.java
+++ src/com/ck/pre/ds/Employee.java
@@ -8,4 +8,18 @@ public class Employee {
 		Employee e = new Employee();
 		e.a = 10;
 	}
+
+int fib(int n) {
+	int a = 0, b = 1, c;
+	if (n == 0 || n == 1) {
+		return n;
+	}
+
+	for (int i = 2; i <= n; i++) {
+		c = a + b;
+		a = b;
+		b = c;
+	}
+	return b;
+}
 }
