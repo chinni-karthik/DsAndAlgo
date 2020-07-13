904682cf620854c945c64dc37fbc7936e614a7d2
chinni-karthik <chinni.kaarthik@gmail.com>
2020-07-13 21:48:35 +0530

    udpating notes

---
+++ src/com/ck/arrays/ArrayTraverse.java
@@ -0,0 +1,10 @@
+package com.ck.arrays;
+
+public class ArrayTraverse {
+	public static void main(String[] args) {
+		int[] intArray = { 11, 9, 17, 89, 1, 90, 19, 5, 3, 23, 43, 99 };
+		for (int i = 0; i < intArray.length; i++) {
+			System.out.println(intArray[i]);
+		}
+	}
+}
