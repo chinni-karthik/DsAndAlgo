5e4f307fbe5ab33b9cb3f084a921ded79b2e3078
chinni-karthik <chinni.kaarthik@gmail.com>
2021-06-29 10:52:04 +0530

    Starting interview questions. Need to finish asap
    Starting interview questions. Need to finish asap

---
+++ src/com/ck/interview/questions/SequentialStreamDemo.java
@@ -0,0 +1,13 @@
+package com.ck.interview.questions;
+
+import java.util.Arrays;
+import java.util.List;
+
+class SequentialStreamDemo {
+	public static void main(String[] args) {
+		// create a list
+		List<String> list = Arrays.asList("Hello ", "G", "E", "E", "K", "S!");
+
+		list.stream().forEach(System.out::print);
+	}
+}
---
+++ src/com/ck/interview/questions/TransientExample.java
@@ -0,0 +1,12 @@
+package com.ck.interview.questions;
+
+import java.io.Serializable;
+
+// A sample class that uses transient keyword to skip their serialization.
+class TransientExample implements Serializable {
+   transient int age;
+   // serialize other fields
+   private String name;
+   private String address;
+   // other code
+}
